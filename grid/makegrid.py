86e4d400acfcd290a5d361356ebcb0bc55e51253
Graziano Giuliani <ggiulian@ictp.it>
2025-04-08 12:16:06 +0200

    Accept SCRIP grid input

--- grid/makegrid.py
+++ grid/makegrid.py
@@ -88,6 +88,7 @@ except:
 
 ofile = config["ofile"]
 grid_type = config["grid_type"]
+output_2d = config["output_2d"]
 
 if grid_type == "regular_latlon":
     resolution = config["regular_latlon"]["resolution"]
@@ -136,6 +137,27 @@ elif grid_type == "orca_grid":
     toprightlon = glamu[0,1:-1,1:-1]
     topleftlon = glamu[0,1:-1,0:-2]
     cellarea = dxf*dyf
+elif grid_type == "scrip_grid":
+    infile = config["scrip_grid"]["gridfile"]
+    ds = Dataset(infile,'r')
+    shape = ds.variables['grid_dims'][:]
+    nlat,nlon = shape.tolist( )
+    centerlon = ds.variables['grid_center_lon'][:].data.reshape(shape)
+    centerlat = ds.variables['grid_center_lat'][:].data.reshape(shape)
+    clat = ds.variables['grid_corner_lat']
+    clon = ds.variables['grid_corner_lon']
+    bottomleftlat = clat[:,0].reshape(shape)
+    bottomrightlat = clat[:,1].reshape(shape)
+    toprightlat = clat[:,2].reshape(shape)
+    topleftlat = clat[:,3].reshape(shape)
+    bottomleftlon = clon[:,0].reshape(shape)
+    bottomrightlon = clon[:,1].reshape(shape)
+    toprightlon = clon[:,2].reshape(shape)
+    topleftlon = clon[:,3].reshape(shape)
+    ds.close( )
+    cellarea = compute_areas(centerlat,bottomleftlat,bottomrightlat,
+            toprightlat,topleftlat,centerlon,bottomleftlon,bottomrightlon,
+            toprightlon,topleftlon)
 
 with Dataset(ofile, "w") as dst:
 
@@ -143,56 +165,108 @@ with Dataset(ofile, "w") as dst:
     dst.gridtype = 'cell'
     dst.datetime = datetime.datetime.now(datetime.UTC).isoformat( )
 
-    # Define dimension
-    dst.createDimension("grid_rank",2)
-    dst.createDimension("grid_size",nlon*nlat)
-    dst.createDimension("grid_xsize",nlon)
-    dst.createDimension("grid_ysize",nlat)
-    dst.createDimension("grid_corners",4)
+    if output_2d:
+        # Define dimension
+        dst.createDimension("grid_rank",2)
+        dst.createDimension("grid_size",nlon*nlat)
+        dst.createDimension("grid_xsize",nlon)
+        dst.createDimension("grid_ysize",nlat)
+        dst.createDimension("grid_corners",4)
 
-    # Define variables
-    grid_dims = dst.createVariable("grid_dims", 'i4', ["grid_rank",])
-    grid_center_lon = dst.createVariable("grid_center_lon", 'f4',
-            ["grid_ysize", "grid_xsize"])
-    grid_center_lon.standard_name = "longitude"
-    grid_center_lon.long_name = "Longitude"
-    grid_center_lon.units = "degrees_east"
-    grid_center_lon.bounds = "grid_corner_lon"
-    grid_center_lat = dst.createVariable("grid_center_lat", 'f4',
-            ["grid_ysize", "grid_xsize"])
-    grid_center_lat.standard_name = "latitude"
-    grid_center_lat.long_name = "Latitude"
-    grid_center_lat.units = "degrees_north"
-    grid_center_lat.bounds = "grid_corner_lat"
-    grid_cell_area = dst.createVariable("cell_area", 'f4',
-            ["grid_ysize", "grid_xsize"])
-    grid_cell_area.standard_name = "cell_area"
-    grid_cell_area.long_name = "Horizontal Area of gridcell"
-    grid_cell_area.units = "m2"
-    grid_cell_area.coordinates = "grid_center_lat grid_center_lon"
-    grid_corner_lon = dst.createVariable("grid_corner_lon", 'f4',
-            ["grid_ysize", "grid_xsize", "grid_corners"])
-    grid_corner_lon.standard_name = "longitude_bounds"
-    grid_corner_lon.long_name = "Longitude Bounds"
-    grid_corner_lon.units = "degrees_north"
-    grid_corner_lon.coordinates = "grid_center_lat grid_center_lon"
-    grid_corner_lat = dst.createVariable("grid_corner_lat", 'f4',
-            ["grid_ysize", "grid_xsize", "grid_corners"])
-    grid_corner_lat.standard_name = "latitude_bounds"
-    grid_corner_lat.long_name = "Latitude Bounds"
-    grid_corner_lat.units = "degrees_north"
-    grid_corner_lat.coordinates = "grid_center_lat grid_center_lon"
+        # Define variables
+        grid_dims = dst.createVariable("grid_dims", 'i4', ["grid_rank",])
+        grid_center_lon = dst.createVariable("grid_center_lon", 'f4',
+                ["grid_ysize", "grid_xsize"])
+        grid_center_lon.standard_name = "longitude"
+        grid_center_lon.long_name = "Longitude"
+        grid_center_lon.units = "degrees_east"
+        grid_center_lon.bounds = "grid_corner_lon"
+        grid_center_lat = dst.createVariable("grid_center_lat", 'f4',
+                ["grid_ysize", "grid_xsize"])
+        grid_center_lat.standard_name = "latitude"
+        grid_center_lat.long_name = "Latitude"
+        grid_center_lat.units = "degrees_north"
+        grid_center_lat.bounds = "grid_corner_lat"
+        grid_cell_area = dst.createVariable("cell_area", 'f4',
+                ["grid_ysize", "grid_xsize"])
+        grid_cell_area.standard_name = "cell_area"
+        grid_cell_area.long_name = "Horizontal Area of gridcell"
+        grid_cell_area.units = "m2"
+        grid_cell_area.coordinates = "grid_center_lat grid_center_lon"
+        grid_corner_lon = dst.createVariable("grid_corner_lon", 'f4',
+                ["grid_ysize", "grid_xsize", "grid_corners"])
+        grid_corner_lon.standard_name = "longitude_bounds"
+        grid_corner_lon.long_name = "Longitude Bounds"
+        grid_corner_lon.units = "degrees_north"
+        grid_corner_lon.coordinates = "grid_center_lat grid_center_lon"
+        grid_corner_lat = dst.createVariable("grid_corner_lat", 'f4',
+                ["grid_ysize", "grid_xsize", "grid_corners"])
+        grid_corner_lat.standard_name = "latitude_bounds"
+        grid_corner_lat.long_name = "Latitude Bounds"
+        grid_corner_lat.units = "degrees_north"
+        grid_corner_lat.coordinates = "grid_center_lat grid_center_lon"
 
-    # Write variables in file
-    grid_dims[:] = np.array((nlon,nlat))
-    grid_center_lat[:] = centerlat
-    grid_center_lon[:] = centerlon
-    grid_corner_lat[:,:,0] = bottomleftlat
-    grid_corner_lat[:,:,1] = bottomrightlat
-    grid_corner_lat[:,:,2] = toprightlat
-    grid_corner_lat[:,:,3] = topleftlat
-    grid_corner_lon[:,:,0] = bottomleftlon
-    grid_corner_lon[:,:,1] = bottomrightlon
-    grid_corner_lon[:,:,2] = toprightlon
-    grid_corner_lon[:,:,3] = topleftlon
-    grid_cell_area[:] = cellarea
+        # Write variables in file
+        grid_dims[:] = np.array((nlon,nlat))
+        grid_center_lat[:] = centerlat
+        grid_center_lon[:] = centerlon
+        grid_corner_lat[:,:,0] = bottomleftlat
+        grid_corner_lat[:,:,1] = bottomrightlat
+        grid_corner_lat[:,:,2] = toprightlat
+        grid_corner_lat[:,:,3] = topleftlat
+        grid_corner_lon[:,:,0] = bottomleftlon
+        grid_corner_lon[:,:,1] = bottomrightlon
+        grid_corner_lon[:,:,2] = toprightlon
+        grid_corner_lon[:,:,3] = topleftlon
+        grid_cell_area[:] = cellarea
+    else:
+        # Define dimension
+        dst.createDimension("grid_rank",2)
+        dst.createDimension("grid_size",nlon*nlat)
+        dst.createDimension("grid_corners",4)
+
+        # Define variables
+        grid_dims = dst.createVariable("grid_dims", 'i4', ["grid_rank",])
+        grid_center_lon = dst.createVariable("grid_center_lon", 'f4',
+                ["grid_size"])
+        grid_center_lon.standard_name = "longitude"
+        grid_center_lon.long_name = "Longitude"
+        grid_center_lon.units = "degrees_east"
+        grid_center_lon.bounds = "grid_corner_lon"
+        grid_center_lat = dst.createVariable("grid_center_lat", 'f4',
+                ["grid_size"])
+        grid_center_lat.standard_name = "latitude"
+        grid_center_lat.long_name = "Latitude"
+        grid_center_lat.units = "degrees_north"
+        grid_center_lat.bounds = "grid_corner_lat"
+        grid_cell_area = dst.createVariable("cell_area", 'f4',
+                ["grid_size"])
+        grid_cell_area.standard_name = "cell_area"
+        grid_cell_area.long_name = "Horizontal Area of gridcell"
+        grid_cell_area.units = "m2"
+        grid_corner_lon = dst.createVariable("grid_corner_lon", 'f4',
+                ["grid_size", "grid_corners"])
+        grid_corner_lon.standard_name = "longitude_bounds"
+        grid_corner_lon.long_name = "Longitude Bounds"
+        grid_corner_lon.units = "degrees_north"
+        grid_corner_lat = dst.createVariable("grid_corner_lat", 'f4',
+                ["grid_size", "grid_corners"])
+        grid_corner_lat.standard_name = "latitude_bounds"
+        grid_corner_lat.long_name = "Latitude Bounds"
+        grid_corner_lat.units = "degrees_north"
+
+        # Write variables in file
+        grid_dims[:] = np.array((nlon,nlat))
+        grid_center_lat[:] = centerlat.flatten( )
+        grid_center_lon[:] = centerlon.flatten( )
+        grid_corner_lat[:,0] = bottomleftlat.flatten( )
+        grid_corner_lat[:,1] = bottomrightlat.flatten( )
+        grid_corner_lat[:,2] = toprightlat.flatten( )
+        grid_corner_lat[:,3] = topleftlat.flatten( )
+        grid_corner_lon[:,0] = bottomleftlon.flatten( )
+        grid_corner_lon[:,1] = bottomrightlon.flatten( )
+        grid_corner_lon[:,2] = toprightlon.flatten( )
+        grid_corner_lon[:,3] = topleftlon.flatten( )
+        grid_cell_area[:] = cellarea.flatten( )
+
+print('Done')
